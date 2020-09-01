549506714738ca9fa8a49dd154b6ba7b7fafeee1
vbs000 <vbs_000@126.com>
2020-09-01 22:42:36 +0800

    add rolelist

--- src/views/system/Role/RoleList.vue
+++ src/views/system/Role/RoleList.vue
@@ -1,15 +1,134 @@
 <template>
-    <div>
-角色
-    </div>
+   <el-main>
+       <!-- 搜索框 -->
+       <el-form size="mini" :model="roleForm" label-width="80px">
+           <el-row>
+               <el-col :span="5">
+                    <el-form-item label="名称">
+                        <el-input v-model="roleForm.roleName" placeholder="请输入角色名称"></el-input>
+                    </el-form-item>
+               </el-col>
+                <el-button size="mini" type="primary" icon="el-icon-search" class="btn-left">搜索</el-button>
+                <el-button size="mini" type="primary" icon="el-icon-plus" >新增</el-button>
+           </el-row>
+           
+       </el-form>
+
+       <!-- 数据表格 
+       :data 表格数据绑定
+       height 表格高度
+       只要在el-table元素中定义了height属性，即可实现固定表头的表格，而不需要额外的代码。
+       border 表格边框
+       prop 属性需要跟表格数据对应
+        -->
+        
+            <el-table
+            :data="tableData"
+            size="mini"
+            :height="tableHeight"
+            border
+            style="width: 100%">
+            <el-table-column
+                prop="date"
+                label="日期"
+                >
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="姓名"
+                >
+            </el-table-column>
+            <el-table-column
+                prop="address"
+                label="地址">
+            </el-table-column>
+            </el-table>
+            <!-- 分页组件
+            size-change page size改变时调用
+            current-change 页数改变时调用
+            current-page 当前页
+            page-size 页容量
+            total 总条数
+             -->
+            <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage1"
+            :page-size="100"
+            layout="total, prev, pager, next"
+            :total="tableData.length">
+            </el-pagination>
+      
+
+
+   </el-main>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            //page size改变时调用
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            //页数改变时调用
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            }
+        },
+        data(){
+            return{
+                //搜索框数据绑定
+                roleForm:{
+                    roleName:""
+                },
+                //表格高度
+                tableHeight:window.innerHeight,
+                //表格数据
+                 tableData: [{
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-08',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-06',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                    date: '2016-05-07',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                    }]
+            }
+        },
+        mounted() {
+            this.$nextTick(() => {
+            this.tableHeight = window.innerHeight - 220; //后面的50：根据需求空出的高度，自行调整
+            });
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+.btn-left{
+    margin-left: 30px;
+}
+.el-main{
+    padding-top: 5px !important;
+}
 </style>
