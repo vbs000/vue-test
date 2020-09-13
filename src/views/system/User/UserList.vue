81dbd71f6221c175226e5bf619b1e2a881e9b869
vbs000 <vbs_000@126.com>
2020-09-13 19:01:42 +0800

    complete user list tree

--- src/views/system/User/UserList.vue
+++ src/views/system/User/UserList.vue
@@ -1,6 +1,17 @@
 <template>
     <el-container>
-        <el-aside width="200px">部门树</el-aside>
+        <el-aside width="200px">
+             <div class="ub cross-center" style="margin-left:5px;font-size:16px;margin-top:5px;">
+                <i class="el-icon-s-grid"></i>
+                <span style="margin-left:3px;">组织机构</span>
+            </div>
+            <tree
+            style="padding-left: 0px;padding-top: 5px;"
+            :nodes="nodes"
+            :setting="setting"
+            @onCreated="handleCreated"
+            />
+        </el-aside>
         <el-main>
             <el-form size="mini" :model="searchForm" ref="searchForm" label-width="80px">
               <el-row>
@@ -69,8 +80,25 @@
 </template>
 
 <script>
+    //引入ztree组件
+    import tree from "vue-giant-tree";
     export default {
+        //注册组件
+        components:{
+            tree
+        },
         methods: {
+            //ztree树点击事件回调函数
+            ztreeOnClick(event, treeId, treeNode){
+                console.log(event);
+                console.log(treeId);
+                console.log(treeNode);
+            },
+            //树创建成功后调用
+            handleCreated(obj){
+                this.ztreeObj = obj;
+                obj.expandAll(true);
+            },
             //编辑事件
             handleEdit(index,row){
                 console.log(index);
@@ -97,6 +125,99 @@
         },
         data() {
             return {
+                //树插件配置
+                ztreeObj: null,
+                setting: {
+                    view: {
+                        showLine: true,//连接线
+                        showIcon: false,
+                        fontCss: { "font-size": "12px", color: "#333" }
+                    },
+                    //设置这里会显示复选框
+                    // check: {
+                    // enable: true
+                    // },
+                    data: {
+                        simpleData: {
+                            enable: true,
+                            idKey: "id",
+                            pIdKey: "pid",
+                            rootPId: "0"
+                        }
+                    },
+                    //配置回调函数
+                    callback: {
+                        //树的点击事件
+                        onClick: this.ztreeOnClick
+                    }
+                },
+                nodes: [{
+                "id": "1000000362292826",
+                "pid": "1000001251633881",
+                "likeId": "0,100000177618509910000012516338811000000362292826",
+                "parentName": "销售部门",
+                "manager": null,
+                "name": "销售1",
+                "deptCode": "",
+                "deptAddress": "",
+                "deptPhone": "",
+                "orderNum": 0
+                }, {
+                "id": "1000001251633881",
+                "pid": "1000001776185099",
+                "likeId": "0,10000017761850991000001251633881",
+                "parentName": "秘咖科技有限公司",
+                "manager": null,
+                "name": "销售部门",
+                "deptCode": null,
+                "deptAddress": null,
+                "deptPhone": null,
+                "orderNum": null
+                }, {
+                "id": "1000001341234088",
+                "pid": "1000001776185099",
+                "likeId": "0,1000001776185099",
+                "parentName": "秘咖网络科技有限公司",
+                "manager": null,
+                "name": "人才管理部1",
+                "deptCode": "RCGL",
+                "deptAddress": "",
+                "deptPhone": "",
+                "orderNum": 0
+                }, {
+                "id": "1000001620535597",
+                "pid": "1000001776185099",
+                "likeId": "0,10000017761850991000001620535597",
+                "parentName": "秘咖网络科技有限公司",
+                "manager": null,
+                "name": "软件研发部",
+                "deptCode": null,
+                "deptAddress": null,
+                "deptPhone": null,
+                "orderNum": null
+                }, {
+                "id": "1000001776185099",
+                "pid": "0",
+                "likeId": "0,1000001776185099",
+                "parentName": "顶级部门",
+                "manager": null,
+                "name": "秘咖网络科技有限公司",
+                "deptCode": null,
+                "deptAddress": null,
+                "deptPhone": null,
+                "orderNum": null
+                }, {
+                "id": "1000002097176073",
+                "pid": "1000001776185099",
+                "likeId": "0,10000017761850991000002097176073",
+                "parentName": "秘咖网络科技有限公司",
+                "manager": "464156",
+                "name": "售后服务部",
+                "deptCode": "SHFWB",
+                "deptAddress": "昆明",
+                "deptPhone": "18687171906",
+                "orderNum": null
+                }],
                 searchForm:{
                     username:'',
                     phone:'',
@@ -135,7 +256,8 @@
                 date: '2016-05-07',
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1518 弄'
-                }]
+                }],
+                
             }
         }
     }
