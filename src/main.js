bf9994fe2471f475aedbca4eb0230faa46b8e1f6
vbs000 <vbs_000@126.com>
2020-11-22 21:12:55 +0800

    add token verify

--- src/main.js
+++ src/main.js
@@ -24,49 +24,88 @@ axios.interceptors.request.use(config=>{
     //以application/json形式提交
     config.headers['Content-Type'] =  'application/json'
   }
+  // 为请求头添加token字段
+  config.headers.token = sessionStorage.getItem('token')
   return config;
 })
+
 //进入路由之前执行
-router.beforeEach((to,from,next) =>{
-  //to 将要进入的路由
-  //from 将要离开的路由
-  //next 让路由继续执行
+router.beforeEach((to, from, next) => {
+  //to   将要进入的路由
+  //from  将要离开的路由
+  //next   让路由继续执行
+
   //获取当前打开的选项卡
   store.commit('getTabs');
   //设置当前激活的选项卡
-  store.commit('setActiveTabs',to.name);
-  //解决刷新之后菜单数据不存在的问题
+  store.commit('setActiveTabs', to.name);
 
-  let menuData = sessionStorage.getItem('menuList');
-  if(to.path === '/login'){
-    if(menuData){
-      next({path:'/home'});
+  let token = sessionStorage.getItem('token');
+  if(to.path ==='/login'){
+    if(token){
+      next({path:'/home'})
     }else{
       next();
     }
   }else{
-    if(!menuData && to.name !== 'login'){
-      next({path:'/login'});
+    if(!token && to.name !== 'login'){
+      next({path:'/login'})
     }else{
       if(store.state.MenuStore.menu_data.length == 0){
         store.commit('getMenuList',router);
-        next({path:to.path});
+        if(to.path =="/"){
+          next({path:'/home'})
+        }else{
+          next({path:to.path});
+        }
       }else{
         next();
       }
     }
   }
-
-  // if(store.state.MenuStore.menu_data.length == 0 ){
-  //   store.commit('getMenuList',router);
-  //   next({path:to.path});
-  // }else{
-  //   next();
-  // }
-  
-  
-
 })
+
+//进入路由之前执行
+// router.beforeEach((to,from,next) =>{
+//   //to 将要进入的路由
+//   //from 将要离开的路由
+//   //next 让路由继续执行
+//   //获取当前打开的选项卡
+//   store.commit('getTabs');
+//   //设置当前激活的选项卡
+//   store.commit('setActiveTabs',to.name);
+//   //解决刷新之后菜单数据不存在的问题
+
+//   let menuData = sessionStorage.getItem('menuList');
+//   if(to.path === '/login'){
+//     if(menuData){
+//       next({path:'/home'});
+//     }else{
+//       next();
+//     }
+//   }else{
+//     if(!menuData && to.name !== 'login'){
+//       next({path:'/login'});
+//     }else{
+//       if(store.state.MenuStore.menu_data.length == 0){
+//         store.commit('getMenuList',router);
+//         next({path:to.path});
+//       }else{
+//         next();
+//       }
+//     }
+//   }
+
+//   // if(store.state.MenuStore.menu_data.length == 0 ){
+//   //   store.commit('getMenuList',router);
+//   //   next({path:to.path});
+//   // }else{
+//   //   next();
+//   // }
+  
+  
+
+// })
 
 new Vue({
   router,
